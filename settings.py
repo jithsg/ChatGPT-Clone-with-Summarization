3225ac4719c7098a85ce446615e322ed0d31da8d
Jithish J <135303624+jithsg@users.noreply.github.com>
2023-11-29 19:39:20 +0000

    edit .ipynb

---
+++ settings.py
@@ -0,0 +1,11 @@
+__import__('pysqlite3')
+import sys
+import os
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
